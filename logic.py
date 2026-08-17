b5fc984395a542454f7572731286e41e2e738ac8
cxanvv <nezlockz@gmail.com>
2026-08-17 12:26:55 +0700

    Implement DeltaDateInDays function
    Added a new function to calculate the difference in days between two dates.

--- logic.py
+++ logic.py
@@ -1,6 +1,12 @@
-import json
+from datetime import datetime
 
 def GetDictFromListWithKeyValue(l,key,value):
     for d in l:
         if d[key] == value: return d
     return False
+
+def DeltaDateInDays(start,end):
+    start_date = datetime.strptime(start, "%d/%m/%Y")
+    end_date = datetime.strptime(end, "%d/%m/%Y")
+    delta = end_date - start_date
+    return delta.days
